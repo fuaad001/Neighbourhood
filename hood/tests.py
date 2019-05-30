a014907c3a803bc7417c5b50d85d439c371e91eb
fuaad001 <husseinfuaad@hotmail.com>
2019-05-30 16:51:09 +0300

    Create neighbourhood tests

--- hood/tests.py
+++ hood/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import *
+import datetime as dt
 
-# Create your tests here.
+class NeighbourhoodTestClass(TestCase):
+    def setUp(self):
+        self.user = User(id = 5, username = 'photolee', password = 'Qwerty123', email = 'photolee@gmail.com')
+        self.Neighbourhood = Neighbourhood(id = 5, name = 'fuaadVille', admin = self.user, location = 'moringa')
+
+    def tearDown(self):
+        Neighbourhood.objects.all().delete()
+        User.objects.all().delete()
