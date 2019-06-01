c7bab967274f987bee1429f34c3d3540057134ee
fuaad001 <husseinfuaad@hotmail.com>
2019-06-01 19:42:24 +0300

    Add string class

--- hood/models.py
+++ hood/models.py
@@ -11,14 +11,17 @@ class Neighbourhood(models.Model):
     occupants_count = models.IntegerField(default = '0')
     admin = models.ForeignKey(User, on_delete = models.CASCADE)
 
-    def create_neigbourhood(self):
+    def __str__(self):
+        return self.name
+
+    def create_neighbourhood(self):
         self.save()
 
-    def delete_neigbourhood(self):
+    def delete_neighbourhood(self):
         self.delete()
 
-    def find_neigbourhood(neigbourhood_id):
-        neighbourhood = Neighbourhood.objects.get(id = neigbourhood_id)
+    def find_neighbourhood(neighbourhood_id):
+        neighbourhood = Neighbourhood.objects.get(id = neighbourhood_id)
         return neighbourhood
 
     def update_neighborhood(self, item, value):
@@ -30,15 +33,21 @@ class Neighbourhood(models.Model):
 class User(models.Model):
     name = models.ForeignKey(User, on_delete = models.CASCADE)
     id_number = models.IntegerField()
-    neigbourhood_id = models.ForeignKey(Neighbourhood, on_delete = models.CASCADE)
+    neighbourhood_id = models.ForeignKey(Neighbourhood, on_delete = models.CASCADE)
     email_address = models.EmailField(max_length=254)
     role = models.CharField(max_length=10, choices=[('Ad', 'Admin'), ('Rs', 'Resident')], default='Resident')
+
+    def __str__(self):
+        return self.name.username
 
 class Business(models.Model):
     name = models.CharField(max_length = 100)
     user = models.ForeignKey(User, on_delete = models.CASCADE)
-    neigbourhood_id = models.ForeignKey(Neighbourhood, on_delete = models.CASCADE)
+    neighbourhood_id = models.ForeignKey(Neighbourhood, on_delete = models.CASCADE)
     email_address = models.EmailField(max_length=254)
+
+    def __str__(self):
+        return self.name
 
     def create_business(self):
         self.save()
@@ -61,12 +70,20 @@ class Business(models.Model):
 class Category(models.Model):
     category_name = models.CharField(max_length = 100)
 
+    def __str__(self):
+        return self.category_name
+
 class News(models.Model):
     image_path = models.ImageField(upload_to = 'images/', null =True, blank = True, default = '../static/images/noimage.svg')
     title = models.CharField(max_length = 100)
     category = models.ForeignKey(Category, on_delete = models.CASCADE)
     description = models.TextField(max_length = 300, blank = True, default = 'No description')
     upload_date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    neighbourhood_id = models.ForeignKey(Neighbourhood, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.title
 
 class Service(models.Model):
     image_path = models.ImageField(upload_to = 'images/', null =True, blank = True, default = '../static/images/noimage.svg')
@@ -74,3 +91,7 @@ class Service(models.Model):
     category = models.ForeignKey(Category, on_delete = models.CASCADE)
     description = models.TextField(max_length = 300, blank = True, default = 'No description')
     location = models.CharField(max_length = 100)
+    neighbourhood_id = models.ForeignKey(Neighbourhood, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.title
