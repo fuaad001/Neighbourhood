e70c66033ec8c63719bcdb11a0d12371f986750f
fuaad001 <husseinfuaad@hotmail.com>
2019-06-03 19:00:09 +0300

    Add business, news, category urls

--- hood/urls.py
+++ hood/urls.py
@@ -10,7 +10,13 @@ urlpatterns=[
         url(r'^services$', views.services, name = 'services'),
         url(r'^business$', views.business, name = 'business'),
         url(r'^about$', views.about, name = 'about'),
-        url(r'^user_admin$', views.user_admin, name = 'user_admin')
+        url(r'^user_admin$', views.user_admin, name = 'user_admin'),
+        url(r'^category$', views.category, name = 'category'),
+        url(r'^newneighbourhood$', views.newneighbourhood, name = 'newneighbourhood'),
+        url(r'^newservice$', views.newservice, name = 'newservice'),
+        url(r'^newbusiness$', views.newbusiness, name = 'newbusiness'),
+        url(r'^search$', views.search, name = 'search'),
+        url(r'^change$', views.change, name = 'change'),
     ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
